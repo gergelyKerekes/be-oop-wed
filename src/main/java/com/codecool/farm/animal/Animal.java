3256f28d68cea44e06eb0fa2fc5745ee9e33b864
Gergely Kerekes <kerekes.gergely83@gmail.com>
2021-03-24 16:25:24 +0100

    comments removed

--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -14,12 +14,3 @@ public abstract class Animal {
 
     public abstract void feed();
 }
-/*
-Animal osztály
-Helye: com.codecool.farm.animal package
-Tartalmazza a következő field-et:
-méret int size
-Az állatok mérete nem állítható a constructor-al, alapértelmezetten az állat létrejöttekor 0.
-A méret field-hez tartozik egy getter.
-Minden állatot meg lehet etetni a feed(), ennek megvalósítása állat típusonként különböző.
- */

61282a2944e38dab68f661231839d068a58e4eaa
Gergely Kerekes <kerekes.gergely83@gmail.com>
2021-03-24 15:37:26 +0100

    all tests are green

--- src/main/java/com/codecool/farm/Farm.java
+++ src/main/java/com/codecool/farm/Farm.java
@@ -1,5 +1,48 @@
 package com.codecool.farm;
 
+import com.codecool.farm.animal.Animal;
+
+import java.util.ArrayList;
+import java.util.List;
+
 class Farm {
 
+    List<Animal> animals = new ArrayList<>();
+
+    public Farm(List<Animal> animals) {
+        this.animals = animals;
+    }
+
+    public List<Animal> getAnimals() {
+        return animals;
+    }
+
+    public void feedAnimals() {
+        for (Animal animal : animals) {
+            animal.feed();
+        }
+    }
+
+    public void butcher(Butcher butcher) {
+        animals.removeIf(butcher::canButcher);
+    }
+
+    public boolean isEmpty() {
+        return animals.size() == 0;
+    }
+
+    public List<String> getStatus() {
+        List<String> ret = new ArrayList<>();
+
+
+        for (Animal animal : animals) {
+            String animalString = "";
+            animalString = "There is a " + animal.getSize() +
+                    " sized " + animal.getClass().getSimpleName().toLowerCase() +
+                    " in the farm.";
+            ret.add(animalString);
+        }
+
+        return ret;
+    }
 }
--- src/main/java/com/codecool/farm/animal/Animal.java
+++ src/main/java/com/codecool/farm/animal/Animal.java
@@ -2,4 +2,24 @@ package com.codecool.farm.animal;
 
 public abstract class Animal {
 
+    private int size = 0;
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public abstract void feed();
 }
+/*
+Animal osztály
+Helye: com.codecool.farm.animal package
+Tartalmazza a következő field-et:
+méret int size
+Az állatok mérete nem állítható a constructor-al, alapértelmezetten az állat létrejöttekor 0.
+A méret field-hez tartozik egy getter.
+Minden állatot meg lehet etetni a feed(), ennek megvalósítása állat típusonként különböző.
+ */
